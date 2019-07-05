eb1d1a521f17a32d6b98499be9bcc1bdf313e942
chengyuming <chengyuming@ky-tech.com.cn>
2019-07-05 11:16:04 +0800

    '自动化部署'

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,8 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f https://${token}@${address} master:master
-git push -f https://github.com/cym-git/cym-git.github.io.git master
-# git push -f https://${blog}@${address} master:master
-
+# git push -f https://github.com/cym-git/cym-git.github.io.git master
+git push -f https://${blog}/${address} master:master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
