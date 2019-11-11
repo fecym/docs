00d9a39fd3cbcf399e7c59c8e3cbd3f59a088479
chengyuming <diy_mnb@163.com>
2019-11-11 23:12:45 +0800

    新增Google监控

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -3,7 +3,7 @@
  * @Author: chengyuming
  * @Date: 2019-07-31 21:57:28
  * @LastEditors: chengyuming
- * @LastEditTime: 2019-09-04 15:00:31
+ * @LastEditTime: 2019-11-11 23:11:10
  */
 module.exports = {
   plugins: [
@@ -34,12 +34,15 @@ module.exports = {
     ['@vuepress/medium-zoom', true],
     ['@vuepress/pwa', {
       serviceWorker: true,
-      // popupComponent: 'MySWUpdatePopup',
+      popupComponent: 'MySWUpdatePopup',
       updatePopup: true
       // updatePopup: {
       //   message: "发现新内容可用",
       //   buttonText: "刷新"
       // }
-    }]
+    }],
+    ['@vuepress/google-analytics', {
+      ga: '151995186'  // Google Analytics ID
+    }],
   ]
 }
