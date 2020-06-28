7423fb6fd6f24dea04dead8cec4bb6c196ae8cac
chengyuming <diy_mnb@163.com>
2020-06-28 22:55:35 +0800

    fix: 更新server-worker

--- docs/.vuepress/components/MySWUpdatePopup.vue
+++ docs/.vuepress/components/MySWUpdatePopup.vue
@@ -1,29 +1,18 @@
-<!--
- * @Description: 
- * @Author: chengyuming
- * @Date: 2019-09-04 14:22:50
- * @LastEditors: chengyuming
- * @LastEditTime: 2019-11-11 23:16:01
- -->
 <template>
   <SWUpdatePopup>
-    <div
-      slot-scope="{ enabled, reload, message, buttonText }"
-      class="my-sw-update-popup">
-      {{ message }}<br>
+    <div slot-scope="{ enabled, reload, message, buttonText }" class="my-sw-update-popup">
+      {{ message }}
+      <br />
       <button @click="reload">{{ buttonText }}</button>
     </div>
   </SWUpdatePopup>
 </template>
-
 <script>
 import SWUpdatePopup from '@vuepress/plugin-pwa/lib/SWUpdatePopup.vue'
-
 export default {
   components: { SWUpdatePopup }
 }
 </script>
-
 <style>
 .my-sw-update-popup {
   text-align: right;
@@ -34,9 +23,7 @@ export default {
   font-size: 20px;
   padding: 10px;
   border: 5px solid #3eaf7c;
-  border-radius: 8px;
 }
-
 .my-sw-update-popup button {
   border: 1px solid #fefefe;
 }
--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,41 +1,45 @@
 module.exports = {
   plugins: [
     [
-      '@vuepress/last-updated', {
-        transformer: (timestamp) => {
+      '@vuepress/last-updated',
+      {
+        transformer: timestamp => {
           // 不要忘了安装 moment
-          const moment = require('moment')
+          const moment = require('moment');
           // moment.locale('zh-CN')
-          return moment(timestamp).format('YYYY-MM-DD HH:mm:ss')
-        }
-      }
+          return moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
+        },
+      },
     ],
-    ['@vuepress/plugin-blog', {
-      permalink: '/:regular',
-      frontmatters: [
-        {
-          id: 'tags',
-          keys: ['tags'],
-          path: '/tag/',
-          layout: 'Tags',
-          scopeLayout: 'Tag'
-        },
-        {
-          id: 'categories',
-          keys: ['categories'],
-          path: '/categories/',
-          layout: 'Categories',
-          scopeLayout: 'Category'
-        },
-        {
-          id: 'timeline',
-          keys: ['timeline'],
-          path: '/timeline/',
-          layout: 'TimeLine',
-          scopeLayout: 'TimeLine'
-        }
-      ]
-    }],
+    [
+      '@vuepress/plugin-blog',
+      {
+        permalink: '/:regular',
+        frontmatters: [
+          {
+            id: 'tags',
+            keys: ['tags'],
+            path: '/tag/',
+            layout: 'Tags',
+            scopeLayout: 'Tag',
+          },
+          {
+            id: 'categories',
+            keys: ['categories'],
+            path: '/categories/',
+            layout: 'Categories',
+            scopeLayout: 'Category',
+          },
+          {
+            id: 'timeline',
+            keys: ['timeline'],
+            path: '/timeline/',
+            layout: 'TimeLine',
+            scopeLayout: 'TimeLine',
+          },
+        ],
+      },
+    ],
     // 移除 vssue
     // [
     //   '@vssue/vuepress-plugin-vssue', {
@@ -52,18 +56,25 @@ module.exports = {
     ['@vuepress/nprogress'],
     ['@vuepress/back-to-top'],
     ['@vuepress/medium-zoom', true],
-    ['@vuepress/pwa', {
-      serviceWorker: true,
-      // popupComponent: 'MySWUpdatePopup',
-      // updatePopup: true
-      updatePopup: {
-        message: "发现新内容可用",
-        buttonText: "刷新"
-      }
-    }],
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        // popupComponent: 'MySWUpdatePopup',
+        // updatePopup: true
+        // updatePopup: {
+        //   message: "发现新内容可用",
+        //   buttonText: "刷新"
+        // }
+        updatePopup: {
+          message: 'New content is available.',
+          buttonText: 'Refresh',
+        },
+      },
+    ],
     // 移除 Google 监控
     // ['@vuepress/google-analytics', {
     //   ga: 'UA-151995186-1'  // Google Analytics ID
     // }],
-  ]
-}
+  ],
+};
