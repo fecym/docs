a4de2a9bb1df59d105151b4295bd50da97145fd0
kele_Bingtang <2456019588@qq.com>
2025-09-18 20:01:08 +0800

    docs: 移除个人博主配置并精简代码
    - 删除了 teekConfig 中的 blogger 配置项，包括 avatar、shape、name、slogan 等信息
    - 移除了 post、markdown

--- docs/.vitepress/teekConfig.ts
+++ docs/.vitepress/teekConfig.ts
@@ -4,15 +4,6 @@ import { version } from "vitepress-theme-teek/es/version";
 export const teekConfig = defineTeekConfig({
   sidebarTrigger: true,
   author: { name: "Teeker", link: "https://github.com/Kele-Bingtang" },
-  blogger: {
-    avatar:
-      "https://testingcf.jsdelivr.net/gh/Kele-Bingtang/static/user/avatar1.png",
-    shape: "circle-rotate",
-    name: "天客",
-    slogan: "朝圣的使徒，正在走向编程的至高殿堂！",
-    circleBgImg: "/blog/bg4.webp",
-    color: "#ffffff",
-  },
   footerInfo: {
     theme: {
       name: `Theme By Teek@${version}`,
@@ -25,19 +16,10 @@ export const teekConfig = defineTeekConfig({
   codeBlock: {
     copiedDone: (TkMessage) => TkMessage.success("复制成功！"),
   },
-  post: {
-    showCapture: true,
-  },
   articleShare: { enabled: true },
   vitePlugins: {
     sidebarOption: {
       initItems: false,
-    },
-  },
-  markdown: {
-    demo: {
-      githubUrl:
-        "https://github.com/Kele-Bingtang/vitepress-theme-teek/blob/master/docs",
     },
   },
 });
